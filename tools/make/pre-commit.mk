e018e7c928667d982313291c86a77f9064e0697e
shown <yuluo08290126@gmail.com>
2025-09-20 21:59:11 +0800

    feat: fix precommit container erro (#182)
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>
    Co-authored-by: Huamin Chen <rootfs@users.noreply.github.com>

--- tools/make/pre-commit.mk
+++ tools/make/pre-commit.mk
@@ -1,3 +1,5 @@
+PRECOMMIT_CONTAINER := ghcr.io/vllm-project/semantic-router/precommit:latest
+
 precommit-install:
 	pip install pre-commit
 
@@ -19,6 +21,24 @@ precommit-check:
 		echo "No Go, Rust, JavaScript, Markdown, Yaml, or Python files found to check"; \
 	fi
 
+# Run pre-commit hooks in a Docker container,
+# and you can exec container to run bash for debug.
+# export PRECOMMIT_CONTAINER=ghcr.io/vllm-project/semantic-router/precommit:latest
+# docker run --rm -it \
+#     -v $(pwd):/app \
+#     -w /app \
+#     --name precommit-container ${PRECOMMIT_CONTAINER} \
+#     bash
+# and then, run `pre-commit install && pre-commit run --all-files` command
 precommit-local:
-	docker pull ghcr.io/vllm/semantic-router/precommit:latest
-	docker run --rm -v $$(pwd):/data ghcr.io/vllm-project/semantic-router/precommit:latest pre-commit run --all-files
+	@if ! docker image inspect ${PRECOMMIT_CONTAINER} > /dev/null 2>&1; then \
+		echo "Image not found locally. Pulling..."; \
+		docker pull ${PRECOMMIT_CONTAINER}; \
+	else \
+		echo "Image found locally. Skipping pull."; \
+	fi
+	docker run --rm \
+	    -v $(shell pwd):/app \
+	    -w /app \
+	    --name precommit-container ${PRECOMMIT_CONTAINER} \
+	    bash -c "source ~/.cargo/env && pre-commit install && pre-commit run --all-files"
